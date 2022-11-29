1030557df1b74b38216b636ab5dd42f71100b451
adidam23 <aditi.dam@gmail.com>
2022-11-29 14:21:50 -0500

    front-end

---
+++ main.py
@@ -0,0 +1,136 @@
+# Source for image upload PyQt code:
+# https://stackoverflow.com/questions/60614561/how-to-ask-user-to-input-an-image-in-pyqt5
+# Source for counting dots
+# #https://stackoverflow.com/questions/60603243/detect-small-dots-in-image 
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import cv2 as cv
+import numpy as np
+
+COUNT = 0
+PATH = ""
+
+class Window(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Capturing Coral Tentacles")
+        self.generalLayout = QGridLayout()
+
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+
+        self._createPhoto()
+        self._createButtonsAndLabels(COUNT)
+
+    def _createPhoto(self):
+        self.photo = Template()
+        self.generalLayout.addWidget(self.photo, 0, 0)
+
+    def _createButtonsAndLabels(self, count):
+        self.savedPicButton = QPushButton("Saved Pictures and Counts")
+        
+        self.countButton = QPushButton("Count")
+        
+        self.countLabel = QLabel("Tentacle Count:")
+        self.countDisplay = QLineEdit("{0}".format(count))
+        self.fullExtLabel = QLabel("Fully Extended:")
+        self.fullExtDisplay = QLineEdit("0")
+        self.partExtLabel = QLabel("Partially Extended:")
+        self.partExtDisplay = QLineEdit("0")
+
+        self.addFullMarkerButton = QPushButton("Add 1 Fully Extended Marker")
+        self.addPartMarkerButton = QPushButton("Add 1 Partially Extended Marker")
+        self.removeMarkerButton = QPushButton("Remove Selected Marker")
+
+        self.savedPicButton.setStyleSheet(
+            "border: 3px solid;"
+            "border-top-color: blue;"
+            "border-left-color: blue;"
+            "border-right-color: blue;"
+            "border-bottom-color: blue;"
+            "color: blue;"
+        )
+
+        self.countButton.setStyleSheet(
+            "border: 3px solid;"
+            "border-top-color: red;"
+            "border-left-color: red;"
+            "border-right-color: red;"
+            "border-bottom-color: red;"
+            "color: red;"
+        )
+
+        self.setStyleSheet(
+            "QLabel {color: purple;}"
+        )
+
+
+        self.smallerGridLayout = QGridLayout()
+        self.smallerGridLayout.addWidget(self.countLabel, 0, 0)
+        self.smallerGridLayout.addWidget(self.countDisplay, 0, 1)
+        self.smallerGridLayout.addWidget(self.fullExtLabel, 1, 0)
+        self.smallerGridLayout.addWidget(self.fullExtDisplay, 1, 1)
+        self.smallerGridLayout.addWidget(self.partExtLabel, 2, 0)
+        self.smallerGridLayout.addWidget(self.partExtDisplay, 2, 1)
+        
+        self.smallGridLayout = QGridLayout()
+        self.smallGridLayout.addWidget(self.savedPicButton, 0, 0)
+        self.smallGridLayout.addWidget(self.countButton, 1, 0)
+        self.smallGridLayout.addLayout(self.smallerGridLayout, 2, 0)
+        self.smallGridLayout.addWidget(self.addFullMarkerButton, 3, 0)
+        self.smallGridLayout.addWidget(self.addPartMarkerButton, 4, 0)
+        self.smallGridLayout.addWidget(self.removeMarkerButton, 5, 0)
+
+        self.generalLayout.addLayout(self.smallGridLayout, 0, 1)
+
+class PhotoLabel(QLabel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAlignment(Qt.AlignCenter)
+        self.setText('\n\n (Image will appear here) \n\n')
+        self.setStyleSheet('''
+        QLabel {
+            border: 4px dashed #aaa;
+        }''')
+        
+    def setPixmap(self, *args, **kwargs):
+        super().setPixmap(*args, **kwargs)
+
+class Template(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.photo = PhotoLabel()
+        btn = QPushButton('Browse')
+        btn.clicked.connect(self.open_image)
+        
+        grid = QGridLayout(self)
+        grid.addWidget(btn, 0, 0, Qt.AlignTop)
+        grid.addWidget(self.photo, 1, 0)
+        self.setAcceptDrops(True)
+        self.resize(300, 200)
+
+    def open_image(self, filename=None):
+        if not filename:
+            filename, _ = QFileDialog.getOpenFileName(self, 'Select Photo', QDir.currentPath(), 'Images (*.png *.jpg)')
+            if not filename:
+                return
+            photo_path = str(filename)
+            
+            self.photo.setPixmap(QPixmap(filename))
+        
+        global PATH
+        PATH = str(photo_path)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gui = Window()
+    gui.show()
+    sys.exit(app.exec_())
