cbb4612f137f22f27e90daf619abe8f2b3c44e8f
adidam23 <aditi.dam@gmail.com>
2023-02-14 15:14:59 -0500

    text format

--- coral_run.py
+++ coral_run.py
@@ -16,20 +16,6 @@ path = 'runs'
 if (os.path.isdir(path)):
     shutil.rmtree(path)
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
-img = 'coral_photos/IMG-6268.JPG'
-
-results = model(img)
-print(results)
-print(results.pred[0])
-# print(results.render())
-# results.show()
-# results.crop()
-
-# fig, ax = plt.subplots(figsize=(16, 12))
-# plt.imshow(results.render()[0])
-# plt.show()
-
 def results_to_string(results):
     return_string = ""
     if results.pred[0].shape[0]:
@@ -38,3 +24,18 @@ def results_to_string(results):
             return_string += f"{n}"
     
     return return_string
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+img = 'coral_photos/IMG-6268.JPG'
+
+results = model(img)
+
+formatted_results = results_to_string(results)
+print(formatted_results, "tentacles")
+
+results.show()
+
+# fig, ax = plt.subplots(figsize=(16, 12))
+# plt.imshow(results.render()[0])
+# plt.show()
+
