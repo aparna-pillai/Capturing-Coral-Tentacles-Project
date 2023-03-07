19cafb617a7d16ac0b83002271e5c33b5a269a11
Aparna <apapil23@bergen.org>
2023-03-06 21:34:39 -0500

    3/6

--- test.py
+++ test.py
@@ -416,6 +416,7 @@ class Window(QWidget):
         
                 
                 print(mydb)
+                print(self.list)
                 self.g.close()
                 mydb.close()
                 
