6993f762ddc5bc9723c257941f68e960454847de
adidam23 <aditi.dam@gmail.com>
2023-04-15 18:20:45 -0400

    created login window

---
+++ LoginWindow.py
@@ -0,0 +1,56 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class LoginWindow(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Log In")
+        self.generalLayout = QGridLayout()
+
+        self.label = QLabel("Enter your code")
+        self.codeTextBox = QLineEdit()
+
+        self.smallerGridLayout = QGridLayout()
+        self.smallerGridLayout.addWidget(self.label, 0, 0)
+        self.smallerGridLayout.addWidget(self.codeTextBox, 0, 1)
+        self.generalLayout.addLayout(self.smallerGridLayout, 0, 0)
+
+        self.submitButton = QPushButton("Submit")
+        self.submitButton.clicked.connect(self.checkCode)
+        self.generalLayout.addWidget(self.submitButton, 1, 0)
+
+        self.setLayout(self.generalLayout)
+
+    def checkCode(self):
+        """
+        For the real function:
+        1. Check if the code is in the database.
+        2. If it is, get the name associated with that code.
+        3. Close this login window and open main window with coral counting function.
+            a. The person's name should be displayed at the top right.
+        """
+
+        enteredCode = self.codeTextBox.text()
+        print(enteredCode)
+        msg = QMessageBox()
+
+        if enteredCode == "abc":
+            msg.setWindowTitle("Access Granted :)")
+            msg.setText("Yes, the correct code is abc! Only for testing, though.")
+        else:
+            msg.setWindowTitle("Access Denied :(")
+            msg.setText("Sorry, that is not the correct code.")
+
+        msg.exec_()
+        
+
+# Only for testing
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+
+    sys.exit(app.exec_())
